309ae0e5de9f1c629f82818428d39653a4b40dca
MarcinSzyc <szyc.marcin@gmail.com>
2019-04-02 12:15:05 +0200

    scroll w liscie filmow, APOD API

--- js/app.js
+++ js/app.js
@@ -93,9 +93,42 @@ $(function () {
 
                 $(".filled").append(tr);
             }
-
+        $("tbody tr:odd").addClass("table-info")
     }
-    $("tbody tr:odd").addClass("table-info")
+
+
+
+    // NASA API
+
+    function NASA_API() {
+        var url = 'https://api.nasa.gov/planetary/apod?api_key=E3EAxenYqulQaMNtdVNwjZ6S260MSmDtKhkFknDL';
+
+        $.ajax({
+          url: url,
+          success: function(result){
+          if("copyright" in result) {
+            $("#copyright").text("Image Credits: " + result.copyright);
+          }
+          else {
+            $("#copyright").text("Image Credits: " + "Public Domain");
+          }
+
+          if(result.media_type == "video") {
+            $("#apod_img_id").css("display", "none");
+            $("#apod_vid_id").attr("src", result.url);
+          }
+          else {
+            $("#apod_vid_id").css("display", "none");
+            $("#apod_img_id").attr("src", result.url);
+          }
+
+          $("#reqObject").text(url);
+          $("#returnObject").text(JSON.stringify(result, null, 4));
+          $("#apod_explaination").text(result.explanation);
+          $("#apod_title").text(result.title);
+        }
+        });
+    }
 
     // Funkcja rozwijajaca menu
 
@@ -209,6 +242,8 @@ $(function () {
         });
 
     add_item()
+
+    NASA_API()
 
 });
 
